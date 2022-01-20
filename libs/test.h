c12fc255d2078eaa78083bf801e129da171beadc
xnacly <matteogropp@protonmail.com>
2022-01-20 13:43:32 +0100

    fixed tests

--- libs/test.h
+++ libs/test.h
@@ -5,22 +5,31 @@
 #include <string.h>
 #include "util.h"
 
+int tests_passed = 0;
+int tests_failed = 0;
+
+void print_test_header(char text[]){
+  printf("\e[1m%s\e[0m\n", text);
+}
+
 void int_test_feedback(double x, double y, char text[], char status[]){
-  printf("[%s%s%s] %s \n\tExpected: %f\n\tGot: %f\n", strcmp(status, "Passed") ? ANSI_COLOR_RED : ANSI_COLOR_GREEN, status, ANSI_COLOR_RESET, text, x, y);
+  printf("\t[%s%s%s] %s \n\t\t \e[3mExpected:\e[0m %f\n\t\t \e[3mGot:\e[0m %f\n\n", strcmp(status, "Passed") ? ANSI_COLOR_RED : ANSI_COLOR_GREEN, status, ANSI_COLOR_RESET, text, x, y);
 }
 
 void vec_test_feedback(double vec[], double vec1[], char text[], char status[]){
-  printf("[%s%s%s] %s \n\tExpected: {%f, %f, %f}\n\tGot: {%f, %f, %f}\n", strcmp(status, "Passed") ? ANSI_COLOR_RED : ANSI_COLOR_GREEN, status, ANSI_COLOR_RESET, text, vec[0], vec[1], vec[2], vec1[0], vec1[1], vec1[2]);
+  printf("\t[%s%s%s] %s \n\t\t \e[3mExpected:\e[0m {%f, %f, %f}\n\t\t \e[3mGot:\e[0m {%f, %f, %f}\n\n", strcmp(status, "Passed") ? ANSI_COLOR_RED : ANSI_COLOR_GREEN, status, ANSI_COLOR_RESET, text, vec[0], vec[1], vec[2], vec1[0], vec1[1], vec1[2]);
 }
 
-void check_if_pass_int(double value1, double value2, char text[]){
+void check_if_equal_int(double value1, double value2, char text[]){
   if((value1-value2) < 0.1)
+    tests_passed++;
     return int_test_feedback(value1, value2, text, "Passed");
 
+  tests_failed++;
   int_test_feedback(value1, value2, text, "Failed");
 }
 
-void check_if_pass_vec(double vec[], double vec1[], char text[]){
+void check_if_equal_vec(double vec[], double vec1[], char text[]){
   int status = 0;
   for (int i = 0; i < 3; i++){
     if(vec[i] == vec1[i] && (vec[i]-vec1[i]) < 0.01) {
@@ -29,10 +38,18 @@ void check_if_pass_vec(double vec[], double vec1[], char text[]){
       status = 0;
       break;
   }
-  if(status == 1)
+  if(status == 1){
+    tests_passed++;
     vec_test_feedback(vec, vec1, text, "Passed");
-  else
+    return;
+  } else
+    tests_failed++;
     vec_test_feedback(vec, vec1, text, "Failed");
 
+}
+
+void print_results(void){
+  printf("\nTest suite results:\n");
+  printf("\t%sPassed%s Tests: %d\n\t%sFailed%s Tests: %d\n", ANSI_COLOR_GREEN, ANSI_COLOR_RESET, tests_passed,  ANSI_COLOR_RED, ANSI_COLOR_RESET, tests_failed);
 }
 #endif
--- tests/test.c
+++ tests/test.c
@@ -3,16 +3,21 @@
 
 
 int main(void){
+  printf("Running tests...\n\n");
   double vec[] = {1,2,3};
   double vec1[] = {2,2,2};
 
+  print_test_header("Tests 1:");
   double vlen = vec_length(vec);
   double vlen1 = vec_length(vec1);
-  check_if_pass_int(3.7, vlen, "Test 1.0: length of the vector");
-  check_if_pass_int(3.5, vlen1, "Test 1.1: length of the vector");
+  check_if_equal_int(3.7, vlen, "Test 1.0: length of the vector");
+  check_if_equal_int(3.5, vlen1, "Test 1.1: length of the vector");
 
+  print_test_header("Tests 2:");
   double v[] = {0,0,0};
-  double v_[] = {5,4,5};
+  double v_[] = {3,4,5};
   vec_add(vec, vec1, v);
-  check_if_pass_vec(v_, v, "Test 2.0: addition of two vectors");
+  check_if_equal_vec(v_, v, "Test 2.0: addition of two vectors");
+
+  print_results();
 }
