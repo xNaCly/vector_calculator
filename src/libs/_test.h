b9e489136452d80677ca85bd0e55a2e19fabead5
xnacly <matteogropp@protonmail.com>
2022-01-25 11:49:25 +0100

    moved consts to _consts.h

--- src/libs/_test.h
+++ src/libs/_test.h
@@ -5,17 +5,14 @@
 #include <string.h>
 #include "_util.h"
 
-int TESTS_PASSED = 0;
-int TESTS_FAILED = 0;
-int VERBOSE = 0;
-
 void print_test_header(char text[]){
   printf("%s%s%s\n", ANSI_STYLE_BOLD, text, ANSI_RESET);
 }
 
 void int_test_feedback(double x, double y, char text[], char status[]){
-  printf("%s %s %s %s \n",
-    strcmp(status, "Passed") ? ANSI_COLOR_RED_BG : ANSI_COLOR_GREEN_BG,
+  printf("%s%s %s %s %s \n",
+    ANSI_COLOR_BLACK,
+    strcmp(status, "PASS") == 0 ? ANSI_COLOR_GREEN_BG : ANSI_COLOR_RED_BG,
     status,
     ANSI_RESET,
     text);
@@ -32,7 +29,12 @@ void int_test_feedback(double x, double y, char text[], char status[]){
 }
 
 void vec_test_feedback(int vec[], int vec1[], char text[], char status[]){
-  printf("%s %s %s %s \n", strcmp(status, "Passed") ? ANSI_COLOR_RED_BG : ANSI_COLOR_GREEN_BG, status, ANSI_RESET, text);
+  printf("%s%s %s %s %s \n",
+    ANSI_COLOR_BLACK,
+    strcmp(status, "PASS") == 0 ? ANSI_COLOR_GREEN_BG : ANSI_COLOR_RED_BG,
+    status,
+    ANSI_RESET,
+    text);
   if(VERBOSE)
   printf("\t%sExpected: {%d, %d, %d}%s\n\t%sGot: {%d, %d, %d}%s\n\n",
     ANSI_STYLE_ITALIC,
@@ -51,35 +53,35 @@ void vec_test_feedback(int vec[], int vec1[], char text[], char status[]){
 void check_if_equal_double(double value1, double value2, char text[]){
   if((value1-value2) < 0.1){
     TESTS_PASSED++;
-    int_test_feedback(value1, value2, text, "Passed");
+    int_test_feedback(value1, value2, text, "PASS");
     return;
   }
 
   TESTS_FAILED++;
-  int_test_feedback(value1, value2, text, "Failed");
+  int_test_feedback(value1, value2, text, "FAIL");
 }
 
 void check_if_equal_int(int value1, int value2, char text[]){
   if(value1 == value2){
     TESTS_PASSED++;
-    int_test_feedback(value1, value2, text, "Passed");
+    int_test_feedback(value1, value2, text, "PASS");
     return;
   }
 
   TESTS_FAILED++;
-  int_test_feedback(value1, value2, text, "Failed");
+  int_test_feedback(value1, value2, text, "FAIL");
 }
 
 void check_if_equal_vec(int vec[], int vec1[], char text[]){
   for (int i = 0; i < 3; i++){
     if(vec[i] != vec1[i]) {
       TESTS_FAILED++;
-      vec_test_feedback(vec, vec1, text, "Failed");
+      vec_test_feedback(vec, vec1, text, "FAIL");
       return;
     }
   }
   TESTS_PASSED++;
-  vec_test_feedback(vec, vec1, text, "Passed");
+  vec_test_feedback(vec, vec1, text, "PASS");
 }
 
 void print_results(double time_elabsed){
--- src/libs/_util.h
+++ src/libs/_util.h
@@ -1,22 +1,11 @@
 #ifndef _UTIL_H_INCLUDED
 #define _UTIL_H_INCLUDED
 
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+#include <stdio.h> // printf()
+#include <string.h> //strcmp()
+#include <stdlib.h> // scanf()
+#include "_consts.h"
 
-#define ANSI_COLOR_RED      "\x1b[91m"
-#define ANSI_COLOR_GREEN    "\x1b[92m"
-#define ANSI_COLOR_RED_BG   "\x1b[101m"
-#define ANSI_COLOR_GREEN_BG "\x1b[102m"
-#define ANSI_COLOR_WHITE_BG "\x1b[40m"
-#define ANSI_STYLE_BOLD     "\x1b[1m"
-#define ANSI_STYLE_ITALIC   "\x1b[3m"
-#define ANSI_RESET          "\x1b[0m"
-
-// possible options for the user to select
-const char *OPTIONS[] = {"double vec_length(int vec[])"};
-const char *OPTIONS_DESC[] = {"returns the length of the given vector"};
 
 void throw_error(char text[], int code){
   printf(ANSI_COLOR_RED "\n\nError (%d): %s\n" ANSI_RESET, code, text);
@@ -51,12 +40,21 @@ int get_selection(void){
 }
 
 void print_help(void){
-  printf("Vector helper v0.1\t\nThis program has several helpful methods for working with vectors:\n\t");
+  printf("Vector helper v0.1\nThis program has several helpful methods for working with vectors:\n");
 
   for(int i = 0; i < (int) (sizeof(OPTIONS)/sizeof(OPTIONS[0])); i++){
-    printf("\t[%d]: %s%s%s -> %s\n", i+1, ANSI_COLOR_WHITE_BG, OPTIONS[i], ANSI_RESET, OPTIONS_DESC[i]);
+    printf("\t[%d]: %s%s%s%s\n\t\t%s%s%s\n\n",
+      i+1,
+      ANSI_STYLE_BOLD,
+      ANSI_COLOR_WHITE,
+      OPTIONS[i],
+      ANSI_RESET,
+      ANSI_STYLE_ITALIC,
+      OPTIONS_DESC[i],
+      ANSI_RESET
+      );
   }
 
-  printf("source: https://github.com/xnacly");
+  printf("source: https://github.com/xnacly/vector_calculator\n");
 }
 #endif
