1d0e6b16ca40c2694558c79794f1ae3ad635b05a
xnacly <matteogropp@protonmail.com>
2022-01-25 11:49:39 +0100

    fixed: argument handling [main.c]

--- src/main.c
+++ src/main.c
@@ -1,8 +1,8 @@
 #include "libs/_util.h"
-#include "string.h" // strcmp
+#include "string.h" // strcmp()
 
 int main(int argc, char *argv[]){
-  const int help = argc > 1 ? strcmp("--help", argv[1]) || strcmp("-h", argv[1]): 0;
+  const int help = argc > 1 ? strcmp("--help", argv[1]) == 0 || strcmp("-h", argv[1]) == 0: 0;
   if(help){
     print_help();
     exit(0);
