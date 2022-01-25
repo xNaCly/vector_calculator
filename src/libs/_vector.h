32935a4a39b922a5619b51b8b9e0ea18cc25d563
xnacly <matteogropp@protonmail.com>
2022-01-25 11:48:30 +0100

    refactor

--- src/libs/_vector.h
+++ src/libs/_vector.h
@@ -5,53 +5,53 @@
 
 #include <math.h>
 
-double vec_length(int vec[]){
+double vec_length(int v[]){
   double s = 0;
   for(int i = 0; i < 3; i++){
-    s += pow(vec[i], 2);
+    s += pow(v[i], 2);
   }
   return sqrt(s);
 }
 
-void vec_add(int v1[], int v2[], int result_vector[]){
+void vec_add(int v[], int v1[], int result_vector[]){
   for(int i = 0; i < 3; i++){
-    result_vector[i] = v1[i] + v2[i];
+    result_vector[i] = v[i] + v1[i];
   }
   return;
 }
 
-void vec_sub(int v1[], int v2[], int result_vector[]){
+void vec_sub(int v[], int v1[], int result_vector[]){
   for(int i = 0; i < 3; i++){
-    result_vector[i] = v1[i] - v2[i];
+    result_vector[i] = v[i] - v1[i];
   }
   return;
 }
 
-void vec_mult(int v1[], int factor, int result_vector[]){
+void vec_mult(int v[], int factor, int result_vector[]){
   for(int i = 0; i < 3; i++){
-    result_vector[i] = v1[i] * factor;
+    result_vector[i] = v[i] * factor;
   }
   return;
 }
 
-int scalar_prod(int v1[], int v2[]){
+int scalar_prod(int v[], int v1[]){
   int s = 0;
   for(int i = 0; i < 3; i++){
-    s += v1[i] * v2[i];
+    s += v[i] * v1[i];
   }
   return s;
 }
 
-double angle_between(int v1[], int v2[]){
-  double s = scalar_prod(v1, v2);
-  double bf = vec_length(v1) * vec_length(v2);
+double angle_between(int v[], int v1[]){
+  double s = scalar_prod(v, v1);
+  double bf = vec_length(v) * vec_length(v1);
   return acos(s / bf) * 180.0 / PI;
 }
 
-void vec_prod(int v1[], int v2[], int result_vector[]){
-  result_vector[0] = v1[1] * v2[2] - v1[2] * v2[1];
-  result_vector[1] = v1[2] * v2[0] - v1[0] * v2[2];
-  result_vector[2] = v1[0] * v2[1] - v1[1] * v2[0];
+void vec_prod(int v[], int v1[], int result_vector[]){
+  result_vector[0] = v[1] * v1[2] - v[2] * v1[1];
+  result_vector[1] = v[2] * v1[0] - v[0] * v1[2];
+  result_vector[2] = v[0] * v1[1] - v[1] * v1[0];
   return;
 }
 
