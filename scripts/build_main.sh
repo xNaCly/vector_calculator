f52500f62ed66cb006d0ef9481f7efb87695ba26
xnacly <matteogropp@protonmail.com>
2022-01-25 10:02:44 +0100

    removed: outdated build scripts

--- scripts/build_main.sh
+++
@@ -1,2 +0,0 @@
-echo "Building Main"
-gcc -fdiagnostics-color=always -g ../src/main.c -lm -o ../build/main.out
--- scripts/build_tests.sh
+++
@@ -1,2 +0,0 @@
-echo "Building Tests"
-gcc -fdiagnostics-color=always -g ../src/tests/test.c -lm -o ../build/test.out
