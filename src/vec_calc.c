0dce71d741399f54d95b8dd1be788db269ca6fbd
xnacly <matteogropp@protonmail.com>
2022-01-29 00:32:54 +0100

    added: method calling helper

--- src/vec_calc.c
+++ src/vec_calc.c
@@ -1,15 +1,67 @@
-#include "libs/util/_util.h"
 #include "string.h" // strcmp()
 
-void init_f(int argc, char *argv[])
+#include "libs/util/_util.h"
+#include "libs/vector/_vector.h"
+
+void selection_handler(int sel)
 {
-  handle_arguments(argc, argv);
-  print_manual();
-  printf("Choose an option by providing the coresponding number: ");
+  if (sel == 1)
+  {
+    int vec[] = {0, 0, 0};
+
+    printf("\nVector (separated by ','): ");
+    scanf("%d,%d,%d", &vec[0], &vec[1], &vec[2]);
+
+    double l = vec_length(vec);
+    printf("Length of the vector: %f\n\n", l);
+  }
+  else if (sel == 2)
+  {
+    int vec[] = {0, 0, 0};
+    int vec1[] = {0, 0, 0};
+    int rvec[] = {0, 0, 0};
+
+    printf("\nVector 1 (separated by ','): ");
+    scanf("%d,%d,%d", &vec[0], &vec[1], &vec[2]);
+    printf("Vector 2 (separated by ','): ");
+    scanf("%d,%d,%d", &vec1[0], &vec1[1], &vec1[2]);
+
+    vec_add(vec, vec1, rvec);
+
+    printf("Resulting Vector: {%d,%d,%d}\n\n", rvec[0], rvec[1], rvec[2]);
+  }
+  else if (sel == 3)
+  {
+    int vec[] = {0, 0, 0};
+    int vec1[] = {0, 0, 0};
+    int rvec[] = {0, 0, 0};
+
+    printf("\nVector 1 (separated by ','): ");
+    scanf("%d,%d,%d", &vec[0], &vec[1], &vec[2]);
+    printf("Vector 2 (separated by ','): ");
+    scanf("%d,%d,%d", &vec1[0], &vec1[1], &vec1[2]);
+
+    vec_sub(vec, vec1, rvec);
+
+    printf("Resulting Vector: {%d,%d,%d}\n\n", rvec[0], rvec[1], rvec[2]);
+  }
+  else if (sel == 8)
+  {
+    exit(0);
+  }
+
+  // creates endless loop
+  helper_init_main();
+  int _sel = get_selection();
+
+  selection_handler(_sel);
 }
 
 int main(int argc, char *argv[])
 {
-  init_f(argc, argv);
+  init_main(argc, argv);
+  int sel = get_selection();
+
+  selection_handler(sel);
   return 0;
 }
